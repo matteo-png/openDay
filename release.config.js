11b806303f886cdee5b40dc91e9eb67cc2d95310
matteo-png <matteo.gorris@gmail.com>
2024-10-16 12:25:01 +0200

    fix: correction GitHub pour les labels

--- release.config.js
+++ release.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    branches: ['master'],
-    
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      '@semantic-release/git',
-    ],
-    verifyConditions: ['@semantic-release/github'],
-    github: {
-      labels: ['release'],
-    },
-  };
+  branches: ['master'],
   
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    ['@semantic-release/github', {
+      labels: ['release']
+    }],
+    '@semantic-release/git',
+  ],
+  
+  verifyConditions: ['@semantic-release/github'],
+};
